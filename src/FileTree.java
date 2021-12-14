a2fae1abbeb0d7db053afd39adc210bc26c44a00
sethlvaughn <95641310+sethlvaughn@users.noreply.github.com>
2021-12-13 21:11:46 -0500

    Update FileTree.java

--- src/FileTree.java
+++ src/FileTree.java
@@ -2,129 +2,194 @@
 // Tyler Harwood & Seth Vaughn      //
 // COS225 FINAL PROJECT            //
 // Professor Schotter             //
-// 17 December 2021              //
+// 15 December 2021              //
 //////////////////////////////////
 
 // Imports
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class FileTree {
+class FileTree {
     
-    // Head
+    // head
     private Folder head = null;
 	
-	// Array list containing all leaf nodes.
-	private ArrayList <File> leaves;
+	// Array list containing all leaf nodes
+	private ArrayList<File> leaves = new ArrayList<File>();
 
-	// Array list of user-saved shortcuts.
-	private ArrayList <Folder> shortcuts;
+	// Array list of user-saved shortcuts
+	private ArrayList<Folder> shortcuts;
 
-	// Other Attributes
+	// Other attributes
+
+
 
     // Constructor
-    public FileTree (File f) {
+    FileTree (File f) {
 
-        System.out.println ("FileTree created.");
+        System.out.println ("FileTree Created");
 
         head = new Folder (f);
         head.parent = null;
 
-        File[] headChildren = f.listFiles();
-        for (int i = 0; i < headChildren.length; i++) {
-            build (head, headChildren [i]);
-        }
-    
-    }
 
-    // Internal function that recursively builds the FileTree.
+        File[] headChildren = f.listFiles();
+        for (int i=0; i < headChildren.length; i++)
+            build (head, headChildren [i]);
+     
+    }
+	
+	// Alternate constructor
+	FileTree(){
+
+		System.out.println("Branch FileTree Created");
+
+		int num = 0;
+		// if()
+
+		// String title;
+		// title = title.format("rip%d", num);
+
+
+
+		// File base = new File(title);
+		// File header = new File(base, "header.txt");
+
+		// head = new Folder(base);
+
+	}
+
+// Internal function that recursively builds the File tree
     private void build (Folder currentFolder, File directory) {
 
         File[] children = directory.listFiles();
 
-        if (directory.isFile() && (directory.getParentFile() != null)) {
+        if (directory.isFile() && (directory.getParentFile() != null))
+	{
             currentFolder.files.add (directory);
             return;
         }
 
-        else if (directory.isDirectory()) {
-            
+        else if (directory.isDirectory()) 
+	{
             Folder childFolder = new Folder (directory);
             childFolder.parent = currentFolder;
             childFolder.parent.folders.add (childFolder);
-	        childFolder.set_depth (currentFolder.depth() + 1);
+	    childFolder.set_depth( currentFolder.depth() + 1 );
 
-            for (int i = 0; i < children.length; i++) {
+            for(int i = 0; i < children.length; i++) 
+	    {
                 build (childFolder, children [i]);
             }
 
+	//// Adds leaf nodes to leaves arrayList
+	    if(childFolder.folders.toArray().length == 0
+		&& childFolder.files.toArray().length != 0)
+	    {
+	    	for(int i = 0; i < childFolder.files.toArray().length; i++)
+	    	{
+			// System.out.println(childFolder.files.get(i).getName());
+			leaves.add(childFolder.files.get(i));
+	    	}
+	    }
+	////
         }
         
-        else {
-            System.out.println ("What the hell am I looking at?");
-        }
+        else { System.out.println ("What the hell am I looking at"); }
 
     }
 
-    // METHODS BY SETH VAUGHN
+// METHODS BY SETH VAUGHN
 
-    // Adds a file to a specified folder relative to the folder being viewed.
+// Adds a file to a specified folder relative to the folder being viewed
     void addFile (Folder parent, File f) {
-        parent.files.add (f);
+	parent.files.add (f);   
     }
 
-    // Adds a Folder to a specified folder relative to the folder being viewed.
+// Adds a Folder to a specified folder relative to the folder being viewed
     void addFolder (Folder parent, Folder child) {
-        parent.folders.add (child);
+	parent.folders.add (child);
     }
 
+// navigates to a folder based on a name or relative path
     public Folder cdForward (Folder currentFolder, String name) {
-        return currentFolder.searchForFolder(currentFolder.folders, name);
+
+	String[] path = name.split("/");
+	Folder p = currentFolder;
+	
+	//Loop through path, traversing tree until end. 
+	for(int i = 0; i < path.length; i++){
+
+		if(path[i].equals("..") && p != null) { p = p.parent; }
+		else if(p != null)
+		{ 
+			Folder out = p.searchFor (path[i]);
+			if(out != null) p = out;
+		}
+		else { System.out.format("%s does not exist\n", path[i]); }
+	}
+
+	return p;
+
     }
 
-    // METHODS BY TYLER HARWOOD
 
-	// Prints the File tree recursively.
-	public void print() {
-        print_p (head);
-    }
+// METHODS BY TYLER HARWOOD
 
-	private void print_p (Folder p) {
 
-		if (p.folders.toArray().length == 0) {
+	// Prints the File tree recursively
+	public void print() { print_p(head); }
+	private void print_p(Folder p)
+	{
+
+		if(p.folders.toArray().length == 0)
+		{
 			p.printFiles();
 			return;
 		}
 
-		else if (p.folders.toArray().length > 0) {
-
+		else if(p.folders.toArray().length > 0)
+		{
 			p.printFiles();
 
 			// Recursive print and traversal into folders
-			for (int i = 0; i < p.folders.toArray().length; i++) {
-
-				for (int j = 0; j < p.depth(); j++) {
+			for(int i = 0; i < p.folders.toArray().length; i++)
+			{
+				for(int j = 0; j < p.depth(); j++)
 					System.out.print("\t");
-                }
-				System.out.format ("%s\n", p.folders.get (i).path());
+
+				System.out.format(
+					"%s\n", p.folders.get(i).path()
+				);
 
 				//Recursive call
-				print_p (p.folders.get (i));
-
+				print_p(p.folders.get(i));
 			}
-
 		}
 		
-		else {
-            System.out.println ("What the hell am I looking at?");
-        }
-
+		else{ System.out.println("Something bad happened :^/"); }
 	}
 
+// Setters
+
 	// Adds a given File to the leaf node array list. 
-	void addLeaf (File f) {
-        leaves.add(f);
-    }
+	void addLeaf(File f) { leaves.add(f); }
+
+
+// Getters
+	
+	Folder getHead() { return head; }
+	
+	void printLeaves()
+	{
+		for(int i = 0; i < leaves.toArray().length; i++)
+		{
+			System.out.format("\t%s\n", leaves.get(i).getPath());
+		}
+	}
+
+	// File search()
 
 }
